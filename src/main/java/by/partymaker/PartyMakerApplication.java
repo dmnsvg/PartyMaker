d5fd2d8166aab76971ad22ad5be08128df5bb6e3
11 <11>
2016-07-14 01:41:42 +0300

    hui

---
+++ src/main/java/by/partymaker/PartyMakerApplication.java
@@ -0,0 +1,12 @@
+package by.partymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartyMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartyMakerApplication.class, args);
+	}
+}
---
+++ src/main/resources/db.sql
@@ -0,0 +1,42 @@
+CREATE TABLE account
+(
+  id         VARCHAR(36) PRIMARY KEY,
+  id_contact VARCHAR(36) NOT NULL,
+  name       VARCHAR(45),
+  surname    VARCHAR(45),
+  email      VARCHAR(45) NOT NULL,
+  password   VARCHAR(45) NOT NULL,
+  birthday   TIMESTAMP,
+  avatar_url VARCHAR(350)
+);
+CREATE UNIQUE INDEX account_email_uindex ON account (email);
+
+CREATE TABLE contact
+(
+  id           VARCHAR(36) PRIMARY KEY,
+  phone_number VARCHAR(45),
+  skype        VARCHAR(45),
+  vk           VARCHAR(45),
+  facebook     VARCHAR(45),
+  viber        VARCHAR(45)
+);
+
+CREATE TABLE event
+(
+  id VARCHAR(36) PRIMARY KEY,
+  id_owner VARCHAR(36) NOT NULL,
+  name VARCHAR(45) NOT NULL,
+  description VARCHAR(500),
+  event_time TIMESTAMP NOT NULL,
+  meetup_place VARCHAR(45) NOT NULL,
+  min_people INT,
+  max_people INT
+);
+
+ALTER TABLE account
+  ADD CONSTRAINT account_contact_id_fk
+FOREIGN KEY (id_contact) REFERENCES contact (id);
+
+ALTER TABLE event
+  ADD CONSTRAINT event_account_id_fk
+FOREIGN KEY (id_owner) REFERENCES account (id);
